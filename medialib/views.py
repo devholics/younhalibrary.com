707481a3134e6b79a4a2bdfd6cb2ddf2608bf4fa
Joonhyung Shin <joon46551@gmail.com>
2023-02-15 01:05:39 -0600

    Performance improvement using raw SQL

--- medialib/views.py
+++ medialib/views.py
@@ -11,23 +11,134 @@ import django_filters
 from .models import Creator, ExternalLink, License, FileMedia, YouTubeVideo, Tag
 
 
+TAG_ORDER_QUERY = '''
+SELECT
+    A.*,
+    num_filemedia + num_youtubevideo as num_media
+FROM
+    medialib_tag A
+LEFT JOIN
+    (
+        SELECT
+            B.id,
+            COUNT(C.filemedia_id)
+                FILTER (
+                    WHERE
+                        C.filemedia_id in (
+                            SELECT
+                                id
+                            FROM
+                                medialib_filemedia
+                            WHERE
+                                public AND license_id IS NOT NULL
+                        )
+                ) as num_filemedia
+        FROM
+            medialib_tag B
+        LEFT JOIN
+            medialib_filemedia_tags C
+        ON
+            B.id = C.tag_id
+        GROUP BY
+            B.id
+    ) D
+ON
+    A.id = D.id
+LEFT JOIN
+    (
+        SELECT
+            E.id,
+            COUNT(F.youtubevideo_id)
+                FILTER (
+                    WHERE
+                        F.youtubevideo_id in (
+                            SELECT
+                                id
+                            FROM
+                                medialib_youtubevideo
+                            WHERE
+                                public AND license_id IS NOT NULL
+                        )
+                ) as num_youtubevideo
+        FROM
+            medialib_tag E
+        LEFT JOIN
+            medialib_youtubevideo_tags F
+        ON
+            E.id = F.tag_id
+        GROUP BY
+            E.id
+    ) G
+ON
+    A.id = G.id
+WHERE
+    num_filemedia + num_youtubevideo > 0
+ORDER BY
+    num_media DESC
+'''
+
+CREATOR_ORDER_QUERY = '''
+SELECT
+    A.*,
+    num_filemedia + num_youtubevideo as num_media
+FROM
+    medialib_creator A
+INNER JOIN
+    (
+        SELECT
+            B.id,
+            COUNT(C.id)
+                FILTER (
+                    WHERE
+                        C.public AND C.license_id IS NOT NULL
+                ) as num_filemedia
+        FROM
+            medialib_creator B
+        LEFT JOIN
+            medialib_filemedia C
+        ON
+            B.id = C.creator_id
+        GROUP BY
+            B.id
+    ) D
+ON
+    A.id = D.id
+INNER JOIN
+    (
+        SELECT
+            E.id,
+            COUNT(F.id)
+                FILTER (
+                    WHERE
+                        F.public AND F.license_id IS NOT NULL
+                ) as num_youtubevideo
+        FROM
+            medialib_creator E
+        LEFT JOIN
+            medialib_youtubevideo F
+        ON
+            E.id = F.creator_id
+        GROUP BY
+            E.id
+    ) G
+ON
+    A.id = G.id
+WHERE
+    num_filemedia + num_youtubevideo > 0
+ORDER BY
+    num_media DESC
+'''
+
+
 class ExhibitionMixin:
     tag_limit = settings.MEDIALIB_TAG_LIMIT
     creator_limit = settings.MEDIALIB_CREATOR_LIMIT
 
-    def order_by_media_count(self, qs):
-        return qs.annotate(
-            num_filemedia=Count('filemedia', filter=Q(filemedia__in=FileMedia.objects.displayed())),
-            num_youtubevideo=Count('youtubevideo', filter=Q(youtubevideo__in=YouTubeVideo.objects.displayed()))
-        ).annotate(
-            num_media=F('num_filemedia') + F('num_youtubevideo')
-        ).filter(num_media__gt=0).order_by('-num_media')
-
     def get_creator_queryset(self):
-        return self.order_by_media_count(Creator.objects.all())[:self.creator_limit]
+        return Creator.objects.raw(CREATOR_ORDER_QUERY)[:self.creator_limit]
 
     def get_tag_queryset(self):
-        return self.order_by_media_count(Tag.objects.all())[:self.tag_limit]
+        return Tag.objects.raw(TAG_ORDER_QUERY)[:self.tag_limit]
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
