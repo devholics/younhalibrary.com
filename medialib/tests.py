18f5a592013ac27122a521324689d2fbea68ea7a
Joonhyung Shin <joon46551@gmail.com>
2023-06-06 01:27:57 -0500

    Add fundamental tests

--- medialib/tests.py
+++ medialib/tests.py
@@ -1,10 +1,14 @@
 from django.test import TestCase
 from django.urls import reverse
 
-# Create your tests here.
-
 
 class TestGallery(TestCase):
     def test_index(self):
         response = self.client.get(reverse("filemedia-list"))
+        self.assertEqual(response.status_code, 200)
+
+
+class TestYouTube(TestCase):
+    def test_index(self):
+        response = self.client.get(reverse("youtubevideo-list"))
         self.assertEqual(response.status_code, 200)
