baf1cc323ca17509bab4a2be191736f6508f22fa
Joonhyung Shin <joon46551@gmail.com>
2023-06-06 17:55:30 -0500

    Add fixtures for testing

--- medialib/tests.py
+++ medialib/tests.py
@@ -1,17 +1,26 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from django.urls import reverse
 
 from .models import Creator
 
 
 class TestGallery(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        pass
+    fixtures = ["medialib_gallery_data.json"]
 
     def test_index(self):
         response = self.client.get(reverse("filemedia-list"))
         self.assertEqual(response.status_code, 200)
+
+    @override_settings(MEDIALIB_GALLERY_PAGINATION=5)
+    def test_pagination_duplicate(self):
+        response = self.client.get(reverse("creator-gallery", kwargs={"pk": 1}))
+        self.assertEqual(response.status_code, 200)
+        creator = response.context["creator"]
+        self.assertEqual(creator.media_count(), 8)
+        self.assertEqual(len(response.context["page_obj"]), 5)
+        response = self.client.get(reverse("creator-gallery", kwargs={"pk": 1}), {"page": 2})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context["page_obj"]), 3)
 
 
 class TestYouTube(TestCase):
@@ -32,14 +41,3 @@ class TestCreator(TestCase):
         self.assertEqual(len(response.context["object_list"]), 2)
         response = self.client.get(reverse("creator-detail", kwargs={"pk": self.younha.id}))
         self.assertEqual(response.status_code, 200)
-
-
-class TestTag(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        pass
-
-    def test_index(self):
-        response = self.client.get(reverse("tag-list"))
-        self.assertEqual(response.status_code, 200)
-
