acd3fa29f4bd4db4c5d2d9ba7f2ad18921adc561
Joonhyung Shin <joon46551@gmail.com>
2023-06-07 14:27:19 -0500

    Add YouTube test

--- medialib/tests.py
+++ medialib/tests.py
@@ -1,8 +1,6 @@
 from django.test import TestCase, override_settings
 from django.urls import reverse
 
-from .models import Creator
-
 
 class TestGallery(TestCase):
     fixtures = ["medialib_gallery_data.json"]
@@ -12,7 +10,7 @@ class TestGallery(TestCase):
         self.assertEqual(response.status_code, 200)
 
     @override_settings(MEDIALIB_GALLERY_PAGINATION=5)
-    def test_pagination_duplicate(self):
+    def test_creator_pagination(self):
         response = self.client.get(reverse("creator-gallery", kwargs={"pk": 1}))
         self.assertEqual(response.status_code, 200)
         creator = response.context["creator"]
@@ -24,6 +22,24 @@ class TestGallery(TestCase):
 
 
 class TestYouTube(TestCase):
+    fixtures = ["medialib_youtube_data.json"]
+
     def test_index(self):
         response = self.client.get(reverse("youtubevideo-list"))
         self.assertEqual(response.status_code, 200)
+
+    @override_settings(MEDIALIB_YOUTUBE_PAGINATION=5)
+    def test_creator_pagination(self):
+        response = self.client.get(reverse("creator-youtube", kwargs={"pk": 2}))
+        self.assertEqual(response.status_code, 200)
+        creator = response.context["creator"]
+        self.assertEqual(creator.media_count(), 1)
+
+        response = self.client.get(reverse("creator-youtube", kwargs={"pk": 27}))
+        self.assertEqual(response.status_code, 200)
+        creator = response.context["creator"]
+        self.assertEqual(creator.media_count(), 7)
+        self.assertEqual(len(response.context["page_obj"]), 5)
+        response = self.client.get(reverse("creator-youtube", kwargs={"pk": 27}), {"page": 2})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context["page_obj"]), 2)
