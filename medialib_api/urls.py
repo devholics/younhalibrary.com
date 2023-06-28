7799dec73e7e5a750abe01d6f5cea7f91c3cce5b
Joonhyung Shin <joon46551@gmail.com>
2023-06-28 15:48:11 -0500

    Support for legacy API

--- medialib_api/urls.py
+++ medialib_api/urls.py
@@ -9,6 +9,7 @@ router = DefaultRouter()
 router.register(r'creators', views.CreatorViewSet, basename='creator')
 router.register(r'sources', views.MediaSourceViewSet, basename='source')
 router.register(r'photos', views.PhotoViewSet, basename='photo')
+router.register(r'file_medias', views.PhotoViewSet, basename='file_media')
 router.register(r'youtube_videos', views.YouTubeVideoViewSet, basename='youtube_video')
 router.register(r'tags', views.TagViewSet, basename='tag')
 
